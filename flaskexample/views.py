77eeb541a2f79302a7cf0250eb25d7543cedb7a4
yaschebaro <yasmine.chebaro@gmail.com>
2020-01-01 21:28:14 +0200

    updates

--- flaskexample/views.py
+++ flaskexample/views.py
@@ -6,7 +6,7 @@ query a SQL database from here instead.
 May 2019, Donald Lee-Brown
 """
 
-from flask import render_template
+from flask import render_template, session
 from flaskexample import app
 from flaskexample.a_model import ModelIt
 import pandas as pd
@@ -23,49 +23,88 @@ def homepage():
 def linked_example():
 		return render_template("example_linked.html")
 
-#here's a page that simply displays the births data
-@app.route('/example_dbtable')
-def birth_table_page():
-		births = []
-		# let's read in the first 10 rows of births data - note that the directory is relative to run.py
-		dbname = './flaskexample/static/data/births2012_downsampled.csv'
-		births_db = pd.read_csv(dbname).head(10)
-		# when passing to html it's easiest to store values as dictionaries
-		for i in range(0, births_db.shape[0]):
-				births.append(dict(index=births_db.index[i], attendant=births_db.iloc[i]['attendant'],
-													 birth_month=births_db.iloc[i]['birth_month']))
-		# note that we pass births as a variable to the html page example_dbtable
-		return render_template('/example_dbtable.html', births=births)
+# linking about page
+@app.route('/about')
+def about():
+		return render_template("about.html")
 
-# now let's do something fancier - take an input, run it through a model, and display the output on a separate page
+@app.route('/details')
+def test():
+		user_input = request.args.to_dict()
 
-@app.route('/model_input')
-def birthmodel_input():
-	 return render_template("model_input.html")
+		dbname = './flaskexample/static/data/db.sqlite'
+		conn = sqlite3.connect(dbname)
+		cur = conn.cursor()
+
+		key = '%'+user_input['title']+'%'
+		query = "select * from nsf where AwardTitle like '"+key+"';"
+		result_df = pd.read_sql_query(query, conn)
+
+		results = []
+		results.append(dict(year=result_df.iloc[0]['Year'], title=result_df.iloc[0]['AwardTitle'], 
+				amount=result_df.iloc[0]['AwardAmount'], inv=result_df.iloc[0]['InvestigatorsNames'], 
+				inst=result_df.iloc[0]['Institution'], id=result_df.iloc[0]['AwardID'],
+				effdate=result_df.iloc[0]['AwardEffectiveDate'], expdate=result_df.iloc[0]['AwardExpirationDate'],
+				abstract=result_df.iloc[0]['Abstract']))
+
+		# print(user_input)
+		print(results)
+		# print(resulta_df)
+		return render_template("details.html", results=results)
+
 
 @app.route('/model_output')
 def birthmodel_output():
-	 # pull input fields and store them
-		investigator = request.args.get('investigator')
-		institution = request.args.get('institution')
-		keyword = request.args.get('keyword')
+	 	# pull input fields and store them
+		user_input = request.args.to_dict()
 
 		# read in the data
 		dbname = './flaskexample/static/data/db.sqlite'
 		conn = sqlite3.connect(dbname)
 		cur = conn.cursor()
-		print(keyword, type(keyword))
 
-		key = '%'+str(keyword)+'%'
+		# search from abstract
+		key = '%'+user_input['keyword']+'%'
 		query = "select * from nsf where Abstract like '"+key+"';"
-		# result_keyword = cur.execute("select * from nsf where Abstract like ?", [key]).fetchall()
-		#print(result_keyword, key)
-		result_df = pd.read_sql_query(query, conn)
+		resulta_df = pd.read_sql_query(query, conn)
+
+		# search from investigator
+		keyig = '%'+user_input['investigator']+'%'
+		query = "select * from nsf where InvestigatorsNames like '"+keyig+"';"
+		resultig_df = pd.read_sql_query(query, conn)
+
+		# search from institution
+		keyis = '%'+user_input['institution']+'%'
+		query = "select * from nsf where Institution like '"+keyis+"';"
+		resultis_df = pd.read_sql_query(query, conn)
+
+		# merge the dataframes dropping duplicates
+		tmp = pd.merge(resulta_df, resultig_df, how='inner')
+		result_df = pd.merge(tmp, resultis_df, how='inner')
+		result_df.dropna(inplace=True)
+		result_df['AwardAmount'] = result_df['AwardAmount'].astype(int)
+
+		# print(user_input)
+
+		# search from funding amount
+		if user_input['funding'] == 'lt50K':
+			result_df = result_df[result_df['AwardAmount'] < 50000]
+		if user_input['funding'] == '50K-100K':
+			result_df = result_df[(result_df['AwardAmount'] >= 50000) & (result_df['AwardAmount'] < 100000)]
+		if user_input['funding'] == '100K-500K':
+			result_df = result_df[(result_df['AwardAmount'] >= 100000) & (result_df['AwardAmount'] < 500000)]
+		if user_input['funding'] == '500K-1M':
+			result_df = result_df[(result_df['AwardAmount'] >= 500000) & (result_df['AwardAmount'] < 1000000)]
+		if user_input['funding'] == '1M-5M':
+			result_df = result_df[(result_df['AwardAmount'] >= 1000000) & (result_df['AwardAmount'] < 5000000)]
+		if user_input['funding'] == 'gt5M':
+			result_df = result_df[result_df['AwardAmount'] > 5000000]
+
 
 		results = []
 		for i in range(0, result_df.shape[0]):
-			results.append(dict(year=result_df.Year[i], title=result_df.AwardTitle[i], amount=result_df.AwardAmount[i], inv=result_df.InvestigatorsNames[i],
-				inst=result_df.Institution[i]))
-			# , attendant=result_df.iloc[i]['attendant'],
-							 # birth_month=result_df.iloc[i]['birth_month']))
+			results.append(dict(year=result_df.iloc[i]['Year'], title=result_df.iloc[i]['AwardTitle'], 
+				amount=result_df.iloc[i]['AwardAmount'], inv=result_df.iloc[i]['InvestigatorsNames'], 
+				inst=result_df.iloc[i]['Institution'], id=result_df.iloc[i]['AwardID']))
+
 		return render_template("model_output.html", results=results, nb_results=result_df.shape[0])
